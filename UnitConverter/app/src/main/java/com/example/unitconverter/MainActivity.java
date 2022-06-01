23eb97e72494d14e899f29d339ccde477ac2b634
vicky <vickyrules1705@gmail.com>
2022-06-02 00:17:38 +0530

    main activity ui added

---
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.unitconverter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.About:
+                Toast.makeText(this,    "AboutUS", Toast.LENGTH_SHORT).show();return true;
+            case R.id.Version:
+                Toast.makeText(this, "Version 1.0.1", Toast.LENGTH_SHORT).show(); return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
