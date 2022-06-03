e37d3cf21c536c9a58032a5016ab1e6bc7d4f61c
vicky <vickyrules1705@gmail.com>
2022-06-03 21:14:23 +0530

    selectUnit activity implementaion unstable  added

--- UnitConverter/app/src/main/java/com/example/unitconverter/SelectUnitActivity.java
+++ UnitConverter/app/src/main/java/com/example/unitconverter/SelectUnitActivity.java
@@ -23,6 +23,53 @@ public class SelectUnitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        setContentView(R.layout.activity_select_unit);
+        getSupportActionBar().setTitle("Select Unit");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        radioGroupMetric  = (RadioGroup) findViewById(R.id.radioGpMatric);
+        radioGroupImperial  = (RadioGroup) findViewById(R.id.radioGpImperial);
+
+        ArrayList <String> metricUnitList = new ArrayList<String>();
+        ArrayList <String> imperailUnitList = new ArrayList<String>();
+        metricUnitList = (ArrayList<String>) getIntent().getSerializableExtra("metricUnits");
+        imperailUnitList = (ArrayList<String>) getIntent().getSerializableExtra("imperialUnits");
+
+        for(String unit : metricUnitList ) {
+
+            RadioButton radioButton = new RadioButton(this.radioGroupImperial.getContext());
+            radioButton.setText(unit);
+            radioButton.setTextColor(this.getResources().getColor(R.color.white));
+            radioButton.setTextDirection(2);
+            radioButton.setTextSize(15);
+            RadioGroup.LayoutParams childParam1 = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.MATCH_PARENT, RadioGroup.LayoutParams.WRAP_CONTENT);
+            radioGroupMetric.addView(radioButton, childParam1);
+        }
+
+
+        for(String unit : imperailUnitList ) {
+
+            RadioButton radioButton = new RadioButton(this.radioGroupImperial.getContext());
+            radioButton.setText(unit);
+            radioButton.setTextColor(this.getResources().getColor(R.color.white));
+            radioButton.setTextDirection(2);
+            radioButton.setTextSize(15);
+            RadioGroup.LayoutParams childParam1 = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.MATCH_PARENT, RadioGroup.LayoutParams.WRAP_CONTENT);
+            radioGroupImperial.addView(radioButton, childParam1);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
