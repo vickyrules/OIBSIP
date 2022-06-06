10241a86b74c03e5b89cf083f3688b38cb45b583
vicky <vickyrules1705@gmail.com>
2022-06-06 22:14:20 +0530

    convertor activity ui updated

--- UnitConverter/app/src/main/java/com/example/unitconverter/SelectUnitActivity.java
+++ UnitConverter/app/src/main/java/com/example/unitconverter/SelectUnitActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.Preference;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -18,15 +22,18 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class SelectUnitActivity extends AppCompatActivity {
-    RadioGroup radioGroupMetric;
-    RadioGroup radioGroupImperial;
-    TextView imperialTxt ,metricTxt;
+    static RadioGroup radioGroupMetric;
+    static RadioGroup radioGroupImperial;
+    TextView imperialTxt, metricTxt;
     View endline;
+
+
+    static ArrayList<String> metricUnitList = new ArrayList<String>();
+    static ArrayList<String> imperailUnitList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         setContentView(R.layout.activity_select_unit);
         getSupportActionBar().setTitle("Select Unit");
@@ -40,46 +47,98 @@ public class SelectUnitActivity extends AppCompatActivity {
         metricTxt = (TextView) findViewById(R.id.metricTxt);
         endline = (View) findViewById(R.id.endViewLine);
 
-        ArrayList<String> metricUnitList = new ArrayList<String>();
-        ArrayList<String> imperailUnitList = new ArrayList<String>();
-
+        metricUnitList.clear();
+        imperailUnitList.clear();
         metricUnitList = (ArrayList<String>) getIntent().getSerializableExtra("metricUnits");
         imperailUnitList = (ArrayList<String>) getIntent().getSerializableExtra("imperialUnits");
 
-        if(imperailUnitList.size() == 0){
+
+        if (imperailUnitList.size() == 0) {
             imperialTxt.setVisibility(View.GONE);
             metricTxt.setVisibility(View.GONE);
             endline.setVisibility(View.GONE);
-
-
         }
 
-        for (String unit : metricUnitList) {
+        // for (String unit : metricUnitList)
+        for (int i = 0; i < metricUnitList.size(); i++) {
 
             RadioButton radioButton = new RadioButton(this.radioGroupImperial.getContext());
-            radioButton.setText(unit);
+            radioButton.setText(metricUnitList.get(i));
             radioButton.setTextColor(this.getResources().getColor(R.color.white));
             radioButton.setTextDirection(2);
             radioButton.setTextSize(16);
             radioButton.setPadding(0, 10, 0, 10);
             RadioGroup.LayoutParams childParam1 = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.MATCH_PARENT, RadioGroup.LayoutParams.WRAP_CONTENT);
             childParam1.setMargins(0, 20, 0, 20);
+
             radioGroupMetric.addView(radioButton, childParam1);
+
+
         }
 
 
-        for (String unit : imperailUnitList) {
+        for (int i = 0; i < imperailUnitList.size(); i++) {
 
             RadioButton radioButton = new RadioButton(this.radioGroupImperial.getContext());
-            radioButton.setText(unit);
+            radioButton.setText(imperailUnitList.get(i));
             radioButton.setTextColor(this.getResources().getColor(R.color.white));
             radioButton.setTextDirection(2);
             radioButton.setTextSize(16);
             radioButton.setPadding(0, 10, 0, 10);
             RadioGroup.LayoutParams childParam1 = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.MATCH_PARENT, RadioGroup.LayoutParams.WRAP_CONTENT);
             childParam1.setMargins(0, 20, 0, 20);
+
             radioGroupImperial.addView(radioButton, childParam1);
+
         }
+
+
+        radioGroupImperial.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent();
+                        int radioButtonID = radioGroupImperial.getCheckedRadioButtonId();
+                        View radioButton = radioGroupImperial.findViewById(radioButtonID);
+                        int idx = radioGroupImperial.indexOfChild(radioButton);
+                        intent.putExtra("metricId",idx);
+                        intent.putExtra("unitText",imperailUnitList.get(idx));
+                        setResult(Activity.RESULT_OK, intent);
+                        finish();
+                    }
+                }, 1000);
+            }
+        });
+
+        radioGroupMetric.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        //returning check radio buttons id
+                        Intent intent = new Intent();
+                        int radioButtonID = radioGroupMetric.getCheckedRadioButtonId();
+                        View radioButton = radioGroupMetric.findViewById(radioButtonID);
+                        int idx = radioGroupMetric.indexOfChild(radioButton);
+                        intent.putExtra("metricId",idx);
+                        intent.putExtra("unitText",metricUnitList.get(idx));
+                        setResult(Activity.RESULT_OK, intent);
+                        finish();
+                    }
+                }, 1000);
+            }
+
+
+        });
+
+
+
 
     }
 
--- UnitConverter/app/src/main/java/com/example/unitconverter/converterActivity.java
+++ UnitConverter/app/src/main/java/com/example/unitconverter/converterActivity.java
@@ -1,17 +1,38 @@
 package com.example.unitconverter;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class converterActivity extends AppCompatActivity {
 
     LinearLayout upperLayout,bottomLayout;
+    TextView unitTextUpper ;
+    TextView unitTextLower;
+    EditText resultTextUpper,resultTextLower;
+    String unitText ;
+    int unitId;
+    int textViewSelected = -1;
+    float textSize;
 
+    private static final int REQUEST_CODE = 101;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,17 +46,46 @@ public class converterActivity extends AppCompatActivity {
 
         upperLayout = (LinearLayout) findViewById(R.id.layoutTextUpper);
         bottomLayout = (LinearLayout) findViewById(R.id.layoutTextLower);
+        unitTextUpper = (TextView) findViewById(R.id.unitTextViewUpper);
+        unitTextLower = (TextView) findViewById(R.id.unitTextViewLower);
+        resultTextUpper = (EditText) findViewById(R.id.resultTextUpper);
+        resultTextLower = (EditText) findViewById(R.id.resultTextLower);
+
+
+        resultTextUpper.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                resultTextUpper.setTextColor(getResources().getColor(R.color.teal_200));
+                resultTextLower.setTextColor(getResources().getColor(R.color.white));
+                return false;
+            }
+        });
+
+       resultTextLower.setOnTouchListener(new View.OnTouchListener() {
+           @Override
+           public boolean onTouch(View view, MotionEvent motionEvent) {
+               resultTextLower.setTextColor(getResources().getColor(R.color.teal_200));
+               resultTextUpper.setTextColor(getResources().getColor(R.color.white));
+
+               return false;
+           }
+       });
+
+
 
         upperLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textViewSelected = 0;
                 startUnitSelect();
+
             }
         });
 
         bottomLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textViewSelected = 1;
                 startUnitSelect();
             }
         });
@@ -46,10 +96,10 @@ public class converterActivity extends AppCompatActivity {
     }
 
     private void startUnitSelect() {
-        Intent intentUnitSelect = new Intent(getApplicationContext(),SelectUnitActivity.class);;
+        Intent intentUnitSelect = new Intent(this,SelectUnitActivity.class);;
         intentUnitSelect.putExtra("metricUnits",getIntent().getSerializableExtra("metricUnits"));
         intentUnitSelect.putExtra("imperialUnits",getIntent().getSerializableExtra("imperialUnits"));
-        startActivity(intentUnitSelect);
+        startActivityForResult(intentUnitSelect,REQUEST_CODE);
     }
 
 
@@ -59,7 +109,28 @@ public class converterActivity extends AppCompatActivity {
         if (item.getItemId() == android.R.id.home) {
             finish(); // close this activity and return to preview activity (if there is any)
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == REQUEST_CODE && data !=null) {
+                unitId = data.getIntExtra("metricId",0);
+                unitText = data.getStringExtra("unitText");
+
+                if(textViewSelected == 0){
+                    unitTextUpper.setText(unitText);
+                }
+                else if (textViewSelected == 1){
+                    unitTextLower.setText(unitText);
+                }
+            }
+
+
+        }
+
     }
 }
