0576c4338e5cdac6f017ec3df9acabf46dc26ff3
vicky <vickyrules1705@gmail.com>
2022-06-07 23:43:34 +0530

    lenght methods added

--- UnitConverter/app/src/main/java/com/example/unitconverter/LengthMethods.java
+++ UnitConverter/app/src/main/java/com/example/unitconverter/LengthMethods.java
@@ -3,162 +3,326 @@ package com.example.unitconverter;
 public class LengthMethods {
 
 
-    //---------------------------------Length--------------------------------
-        //Kilomater to kilometer method
-        public static double kilometer_to_Kilometer ( double value){
+    //---------------------------------Length Conversion--------------------------------
+    //Kilomater to kilometer method
+    public static double kilometer_to_Kilometer(double value) {
         return value;
     }
 
-        //meter to meter method
-        public static double meter_to_Meter ( double value){
+    //meter to meter method
+    public static double meter_to_Meter(double value) {
         return (value);
     }
 
-        //centimeter to centimeter method
-        public static double centimeter_to_Centimeter ( double value){
+    //centimeter to centimeter method
+    public static double centimeter_to_Centimeter(double value) {
         return (value);
     }
 
-        //millimeter to millimeter method
-        public static double millimeter_to_Millimeter ( double value){
+    //millimeter to millimeter method
+    public static double millimeter_to_Millimeter(double value) {
         return (value);
     }
 
-        //inch to inch method
-        public static double inch_to_Inch ( double value){
+    //inch to inch method
+    public static double inch_to_Inch(double value) {
         return (value);
     }
 
-        //mile to mile method
-        public static double mile_to_Mile ( double value){
+    //mile to mile method
+    public static double mile_to_Mile(double value) {
         return (value);
     }
 
-        //foot to foot method
-        public static double foot_to_Foot ( double value){
+    //foot to foot method
+    public static double foot_to_Foot(double value) {
         return (value);
     }
 
-        //yard to yard method
-        public static double yard_to_Yard ( double value){
+    //yard to yard method
+    public static double yard_to_Yard(double value) {
         return (value);
     }
-
 
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //Kilometer to meter method
-    public static  double kilometer_to_Meter(double value){
+    public static double kilometer_to_Meter(double value) {
         return (value * 1000);
     }
 
     //Kilometer to centimeter method
-    public static  double kilometer_to_Centimeter(double value){
+    public static double kilometer_to_Centimeter(double value) {
         return (value * 100000);
     }
 
     //Kilometer to millimeter method
-    public static  double kilometer_to_Millimeter(double value){
+    public static double kilometer_to_Millimeter(double value) {
         return (value * 1000000);
     }
 
     //Kilometer to inch method
-    public static double kilometer_to_Inch(double value){
+    public static double kilometer_to_Inch(double value) {
         return (value * 39370.0787);
     }
 
     //Kilometer to Mile method
-    public static double kilometer_to_Mile(double value){
+    public static double kilometer_to_Mile(double value) {
         return (value * 0.6214);
     }
+
     //Kilometer to foot method
-    public static double kilometer_to_Foot(double value){
+    public static double kilometer_to_Foot(double value) {
         return (value * 3280.8399);
     }
 
     //Kilometer to yard method
-    public static double kilometer_to_Yard(double value){
+    public static double kilometer_to_Yard(double value) {
         return (value * 1093.6133);
     }
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``
 
     //meter to kilometer method
-    public static double meter_to_Kilometer(double value){
+    public static double meter_to_Kilometer(double value) {
         return (value / 1000);
     }
 
     //meter to centimeter method
-    public static double meter_to_Centimeter(double value){
+    public static double meter_to_Centimeter(double value) {
         return (value * 100);
     }
 
     //meter to millimeter method
-    public static double meter_to_Millimeter(double value){
+    public static double meter_to_Millimeter(double value) {
         return (value * 1000);
     }
 
     //meter to inch method
-    public static double meter_to_Inch(double value){
+    public static double meter_to_Inch(double value) {
         return (value * 39.3700787);
     }
 
     //meter to Mile method
-    public static double meter_to_Mile(double value){
+    public static double meter_to_Mile(double value) {
         return (value * 0.0006214);
     }
+
     //meter to Foot method
-    public static double meter_to_Foot(double value){
+    public static double meter_to_Foot(double value) {
         return (value * 3.2808399);
     }
+
     //meter to yard method
-    public static double meter_to_Yard(double value){
+    public static double meter_to_Yard(double value) {
         return (value * 1.0936133);
     }
 
 
-   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
 
 
     //centimeter to kilometer method
-    public static double centimeter_to_Kilometer(double value){
+    public static double centimeter_to_Kilometer(double value) {
         return (value / 100000);
     }
 
     //centimeter to meter method
-    public static double centimeter_to_Meter(double value){
+    public static double centimeter_to_Meter(double value) {
         return (value / 100);
     }
 
     //centimeter to millimeter method
-    public static double centimeter_to_Millimeter(double value){
+    public static double centimeter_to_Millimeter(double value) {
         return (value * 10);
     }
 
     //centimeter to inch method
-    public static double centimeter_to_Inch(double value){
+    public static double centimeter_to_Inch(double value) {
         return (value * 0.393700787);
     }
 
     //centimeter to Mile method
-    public static double centimeter_to_Mile(double value){
+    public static double centimeter_to_Mile(double value) {
         return (value * 0.000006214);
     }
+
     //centimeter to Foot method
-    public static double centimeter_to_Foot(double value){
+    public static double centimeter_to_Foot(double value) {
         return (value * 0.032808399);
     }
+
     //centimeter to yard method
-    public static double centimeter_to_Yard(double value){
+    public static double centimeter_to_Yard(double value) {
         return (value * 0.010936133);
     }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    //millimeter to kilometer method
+    public static double millimeter_to_Kilometer(double value) {
+        return (value / 1000000);
+    }
+
+    //millimeter to meter method
+    public static double millimeter_to_Meter(double value) {
+        return (value / 1000);
+    }
+
+    //millimeter to centimeter method
+    public static double millimeter_to_Centimeter(double value) {
+        return (value / 10);
+    }
+
+    //millimeter to inch method
+    public static double millimeter_to_Inch(double value) {
+        return (value / 25.400000025908);
+    }
+
+    //millimeter to Mile method
+    public static double millimeter_to_Mile(double value) {
+        return (value / 1609269.391696169939);
+    }
+
+    //millimeter to Foot method
+    public static double millimeter_to_Foot(double value) {
+        return (value / 304.799999536704);
+    }
+
+    //millimeter to yard method
+    public static double millimeter_to_Yard(double value) {
+        return (value / 914.399998610112);
+    }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     // inch to kilometer method
-    public static double inch_to_Kilometer(double value){
+    public static double inch_to_Kilometer(double value) {
         return (value / 39370.0787);
+    }
+
+    // inch to meter method
+    public static double inch_to_Meter(double value) {
+        return (value / 39.3700787);
+    }
+
+    // inch to centimeter method
+    public static double inch_to_Centimeter(double value) {
+        return (value / 0.393700787);
+    }
+
+    // inch to millimeter method
+    public static double inch_to_Millimeter(double value) {
+        return (value / 0.0393700787);
+    }
+
+    // inch to mile method
+    public static double inch_to_Mile(double value) {
+        return (value / 63357.062600579336988);
+    }
+
+    // inch to foot method
+    public static double inch_to_Foot(double value) {
+        return (value / 11.9999999695);
+    }
+
+    // inch to yard method
+    public static double inch_to_Yard(double value) {
+        return (value / 35.99999990856);
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // Mile to kilometer method
+    public static double mile_to_Kilometer(double value){
+        return (value / 0.6214);
+    }
+    // Mile to meter method
+    public static double mile_to_Meter(double value){
+        return (value / 0.0006214);
+    }
+    // mile to centimeter method
+    public static double mile_to_Centimeter(double value){
+        return (value / 0.000006214);
+    }
+    // mile to millimeter method
+    public static double mile_to_Millimeter(double value){
+        return (value / 0.0000009214);
+    }
+    // mile to inch method
+    public static double mile_to_Inch(double value){
+        return (value / 0.000015783560);
+    }
+    // mile to foot method
+    public static double mile_to_Foot(double value){
+        return (value / 0.0001894027197);
+    }
+    // mile to yard method
+    public static double mile_to_Yard(double value){
+        return (value / 0.000568208159);
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // foot to kilometer method
+    public static double foot_to_Kilometer(double value){
+        return (value / 3280.8399);
+    }
+    // foot to meter method
+    public static double foot_to_Meter(double value){
+        return (value / 3.2808399);
+    }
+    // foot to centimeter method
+    public static double foot_to_Centimeter(double value){
+        return (value / 0.032808399);
+    }
+    // foot to millimeter method
+    public static double foot_to_Millimeter(double value){
+        return (value / 0.0032808399);
+    }
+    // foot to inch method
+    public static double foot_to_Inch(double value){
+        return (value / 0.083333333545);
+    }
+    // foot to mile method
+    public static double foot_to_Mile(double value){
+        return (value / 5279.755230125523);
+    }
+    // foot to yard method
+    public static double foot_to_Yard(double value){
+        return (value / 2.999999999999);
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // yard to kilometer method
+    public static double yard_to_Kilometer(double value){
+        return (value / 1093.6133);
+    }
+    // yard to meter method
+    public static double yard_to_Meter(double value){
+        return (value / 1.0936133);
+    }
+    // yard to centimeter method
+    public static double yard_to_Centimeter(double value){
+        return (value / 0.010936133);
+    }
+    // yard to millimeter method
+    public static double yard_to_Millimeter(double value){
+        return (value / 0.0010936133);
+    }
+    // yard to inch method
+    public static double yard_to_Inch(double value){
+        return (value / 0.0277777778483);
+    }
+    // yard to mile method
+    public static double yard_to_Mile(double value){
+        return (value / 1759.91841004184100);
+    }
+    // yard to foot method
+    public static double yard_to_Foot(double value){
+        return (value / 0.333333333333);
     }
 
 
