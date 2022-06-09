17cddaee06cb436c2abbf0853006c9b8b413ae5c
vicky <vickyrules1705@gmail.com>
2022-06-09 14:25:03 +0530

    weight  methods  added

--- UnitConverter/app/src/main/java/com/example/unitconverter/WeightMethods.java
+++ UnitConverter/app/src/main/java/com/example/unitconverter/WeightMethods.java
@@ -1,4 +1,215 @@
 package com.example.unitconverter;
 
 public class WeightMethods {
+    //---------------------------------Weight Conversion--------------------------------
+
+    //kg to kg method
+    public static double kg_to_Kg(double value) {
+        return value;
+    }
+    //kg to gr
+    public static double kg_to_G(double value) {
+        return value * 1000;
+    }
+    //kg to mg method
+    public static double kg_to_Mg(double value) {
+        return value * 1000000;
+    }
+    //kg to t
+    public static double kg_to_T(double value) {
+        return value * 0.001;
+    }
+    //kg to lb method
+    public static double kg_to_Lb(double value) {
+        return value * 2.2046226;
+    }
+    //kg to oz
+    public static double kg_to_Oz(double value) {
+        return value * 35.2739619;
+    }
+    //kg to gr method
+    public static double kg_to_Gr(double value) {
+        return value * 15432.3583529;
+    }
+
+
+    //g to kg method
+    public static double g_to_Kg(double value) {
+        return value * 0.001;
+    }
+    //g to g
+    public static double g_to_G(double value) {
+        return value ;
+    }
+    //g to mg method
+    public static double g_to_Mg(double value) {
+        return value * 1000;
+    }
+    //g to t
+    public static double g_to_T(double value) {
+        return value * 0.000001;
+    }
+    //g to lb method
+    public static double g_to_Lb(double value) {
+        return value * 0.0022046226;
+    }
+    //g to oz
+    public static double g_to_Oz(double value) {
+        return value * 0.0352739619;
+    }
+    //g to gr method
+    public static double g_to_Gr(double value) {
+        return value * 15.4323583529;
+    }
+
+
+    //mg to kg method
+    public static double mg_to_Kg(double value) {
+        return value * 0.000001;
+    }
+    //mg to gr
+    public static double mg_to_G(double value) {
+        return value * 0.001;
+    }
+    //mg to mg method
+    public static double mg_to_Mg(double value) {
+        return value;
+    }
+    //mg to t
+    public static double mg_to_T(double value) {
+        return value / 1000000000;
+    }
+    //mg to lb method
+    public static double mg_to_Lb(double value) {
+        return value * 0.0000022046226;
+    }
+    //mg to oz
+    public static double mg_to_Oz(double value) {
+        return value * 0.0000352739619;
+    }
+    //mg to gr method
+    public static double mg_to_Gr(double value) {
+        return value * 0.0154323583529;
+    }
+
+
+    //t to kg method
+    public static double t_to_Kg(double value) {
+        return value * 1000;
+    }
+    //t to g
+    public static double t_to_G(double value) {
+        return value * 1000000;
+    }
+    //t to mg method
+    public static double t_to_Mg(double value) {
+        return value * 1000000000;
+    }
+    //t to t
+    public static double t_to_T(double value) {
+        return value ;
+    }
+    //t to lb method
+    public static double t_to_Lb(double value) {
+        return value * 2204.6226;
+    }
+    //t to oz
+    public static double t_to_Oz(double value) {
+        return value * 35273.9619;
+    }
+    //t to gr method
+    public static double t_to_Gr(double value) {
+        return value * 15432358.3529;
+    }
+
+
+    //lb to kg method
+    public static double lb_to_Kg(double value) {
+        return value / 2.2046226;
+    }
+    //lb to gr
+    public static double lb_to_G(double value) {
+        return value / 2.2046226;
+    }
+    //lb to mg method
+    public static double lb_to_Mg(double value) {
+        return value *453592.374495299;
+    }
+    //lb to t
+    public static double lb_to_T(double value) {
+        return value * 0.000453592374495299;
+    }
+    //lb to lb method
+    public static double lb_to_Lb(double value) {
+        return value ;
+    }
+    //lb to oz
+    public static double lb_to_Oz(double value) {
+        return value * 16.0000001360777086551081;
+    }
+    //lb to gr method
+    public static double lb_to_Gr(double value) {
+        return value * 7000.0000693542724444330171;
+    }
+
+
+    //oz to kg method
+    public static double oz_to_Kg(double value) {
+        return value * 0.028349523164848;
+    }
+    //oz to gr
+    public static double oz_to_G(double value) {
+        return value * 28.349523164848;
+    }
+    //oz to mg method
+    public static double oz_to_Mg(double value) {
+        return value * 28349.523164848;
+    }
+    //oz to t
+    public static double oz_to_T(double value) {
+        return value * 0.000028349523164848;
+    }
+    //oz to lb method
+    public static double oz_to_Lb(double value) {
+        return value * 0.0624999994684474263648;
+    }
+    //oz to oz
+    public static double oz_to_Oz(double value) {
+        return value;
+    }
+    //oz to gr method
+    public static double oz_to_Gr(double value) {
+        return value * 437.5000006137740764588592;
+    }
+
+
+    //gr to kg method
+    public static double gr_to_Kg(double value) {
+        return value * 0.00006479891;
+    }
+    //gr to g
+    public static double gr_to_G(double value) {
+        return value * 0.06479891;
+    }
+    //gr to mg method
+    public static double gr_to_Mg(double value) {
+        return value * 64.79891;
+    }
+    //gr to t
+    public static double gr_to_T(double value) {
+        return value / 15432358.3529;
+    }
+    //gr to lb method
+    public static double gr_to_Lb(double value) {
+        return value * 0.000142857141441366;
+    }
+    //gr to oz
+    public static double gr_to_Oz(double value) {
+        return value * 0.00228571428501529;
+    }
+    //gr to gr method
+    public static double gr_to_Gr(double value) {
+        return value;
+    }
+
 }
