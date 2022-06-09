4d14a0a70366b0b6bd6e9cb0c1d4a338a3c07c44
vicky <vickyrules1705@gmail.com>
2022-06-09 17:30:07 +0530

    volume  methods   added

--- UnitConverter/app/src/main/java/com/example/unitconverter/VolumeMethods.java
+++ UnitConverter/app/src/main/java/com/example/unitconverter/VolumeMethods.java
@@ -1,4 +1,432 @@
 package com.example.unitconverter;
 
 public class VolumeMethods {
+    
+        //---------------------------------Volume Conversion--------------------------------
+
+        //l to l method
+        public static double l_to_L(double value) {
+            return value ;
+        }
+        //l to ml method
+        public static double l_to_Ml(double value) {
+            return value * 1000;
+        }
+        //l to m3 method
+        public static double l_to_M3(double value) {
+            return value * 0.001 ;
+        }
+        //l to Cm3 method
+        public static double l_to_Cm3(double value) {
+            return value * 1000;
+        }
+        //l to mm3 method
+        public static double l_to_Mm3(double value) {
+            return value * 1000000.0;
+        }
+        //l to usgal method
+        public static double l_to_Usgal(double value) {
+            return value * 0.2641720524;
+        }
+        //l to in3 method
+        public static double l_to_In3(double value) {
+            return value * 61.0238445022;
+        }
+        //l to ukgal method
+        public static double l_to_Ukgal(double value) {
+            return value * 0.2199691573;
+        }
+        //l to ft3 method
+        public static double l_to_Ft3(double value) {
+            return value * 0.0353147248;
+        }
+        //l to yard2 method
+        public static double l_to_Yd3(double value) {
+            return value * 0.0013079528;
+        }
+
+
+        //ml to l method
+        public static double ml_to_L(double value) {
+            return value * 0.001;
+        }
+        //ml to ml method
+        public static double ml_to_Ml(double value) {
+            return value ;
+        }
+        //ml to m3 method
+        public static double ml_to_M3(double value) {
+            return value * 0.000001;
+        }
+        //ml to Cm3 method
+        public static double ml_to_Cm3(double value) {
+            return value ;
+        }
+        //ml to mm3 method
+        public static double ml_to_Mm3(double value) {
+            return value * 1000.0;
+        }
+        //ml to Usgal method
+        public static double ml_to_Usgal(double value) {
+            return value * 0.0002641720524;
+        }
+        //ml to in3 method
+        public static double ml_to_In3(double value) {
+            return value * 0.06102384445022;
+        }
+        //ml to ukgal method
+        public static double ml_to_Ukgal(double value) {
+            return value * 0.0002199691573;
+        }
+        //ml to ft3 method
+        public static double ml_to_Ft3(double value) {
+            return value * 0.0000353147248;
+        }
+        //ml to yard2 method
+        public static double ml_to_Yd3(double value) {
+            return value * 0.0000013079528;
+        }
+
+
+        //m3 to l method
+        public static double m3_to_L(double value) {
+            return value * 1000;
+        }
+        //m3 to ml method
+        public static double m3_to_Ml(double value) {
+            return value * 1000000;
+        }
+        //m3 to m3 method
+        public static double m3_to_M3(double value) {
+            return value ;
+        }
+        //m3 to Cm3 method
+        public static double m3_to_Cm3(double value) {
+            return value * 1000000;
+        }
+        //m3 to mm3 method
+        public static double m3_to_Mm3(double value) {
+            return value * 1000000000;
+        }
+        //m3 to usgal method
+        public static double m3_to_Usgal(double value) {
+            return value * 264.1720524;
+        }
+        //m3 to in3 method
+        public static double m3_to_In3(double value) {
+            return value * 61023.8445022;
+        }
+        //m3 to ukgal method
+        public static double m3_to_Ukgal(double value) {
+            return value * 219.9691573;
+        }
+        //m3 to ft3 method
+        public static double m3_to_Ft3(double value) {
+            return value * 35.3147248;
+        }
+        //m3 to yard2 method
+        public static double m3_to_Yd3(double value) {
+            return value * 1.3079528 ;    }
+
+
+        //cm3 to l method
+        public static double cm3_to_L(double value) {
+            return value *  0.001;
+        }
+        //cm3 to m3 method
+        public static double cm3_to_M3(double value) {
+            return value * 0.000001;
+        }
+        //cm3 to ml method
+        public static double cm3_to_Ml(double value) {
+            return value ;
+        }
+        //cm3 to Cm3 method
+        public static double cm3_to_Cm3(double value) {
+            return value ;
+        }
+        //cm3 to mm3 method
+        public static double cm3_to_Mm3(double value) {
+            return value *  1000 ;
+        }
+        //cm3 to usgal method
+        public static double cm3_to_Usgal(double value) {
+            return value * 0.0002641720524;
+        }
+        //cm3 to in3 method
+        public static double cm3_to_In3(double value) {
+            return value * 0.0610238445022;
+        }
+        //cm3 to ukgal method
+        public static double cm3_to_Ukgal(double value) {
+            return value * 0.0002199691573 ;
+        }
+        //cm3 to ft3 method
+        public static double cm3_to_Ft3(double value) {
+            return value * 0.0000353147248 ;
+        }
+        //cm3 to yard2 method
+        public static double cm3_to_Yd3(double value) {
+            return value * 0.0000013079528;
+        }
+
+
+
+        //mm3 to l method
+        public static double mm3_to_L(double value) {
+            return value * 0.000001;
+        }
+        //mm3 to m3 method
+        public static double mm3_to_M3(double value) {
+            return value  * 1E-9;
+        }
+        //mm3 to ml method
+        public static double mm3_to_Ml(double value) {
+            return value * 0.001 ;
+        }
+        //mm3 to Cm3 method
+        public static double mm3_to_Cm3(double value) {
+            return value * 0.001;
+        }
+        //mm3 to mm3 method
+        public static double mm3_to_Mm3(double value) {
+            return value;
+        }
+        //mm3 to usgal method
+        public static double mm3_to_Usgal(double value) {
+            return value * 2.641720524E-7;
+        }
+        //mm3 to in3 method
+        public static double mm3_to_In3(double value) {
+            return value * 0.0000610238445022;
+        }
+        //mm3 to ukgal method
+        public static double mm3_to_Ukgal(double value) {
+            return value  * 2.199691573E-7 ;
+        }
+        //mm3 to ft3 method
+        public static double mm3_to_Ft3(double value) {
+            return value * 3.53147248E-8;
+        }
+        //mm3 to yard2 method
+        public static double mm3_to_Yd3(double value) {
+            return value * 1.3079528E-9;
+        }
+
+
+
+        //usgal to l method
+        public static double usgal_to_L(double value) {
+            return value * 3.7854117834;
+        }
+        //usgal to ml method
+        public static double usgal_to_Ml(double value) {
+            return value * 3785.4117834;
+        }
+        //usgal to m3 method
+        public static double usgal_to_M3(double value) {
+            return value  *  0.0037854117834;
+        }
+        //usgal2 to Cm3 method
+        public static double usgal_to_Cm3(double value) {
+            return value * 3785.4117834;
+        }
+        //usgal to mm3 method
+        public static double usgal_to_Mm3(double value) {
+            return value * 3785411.7834;
+        }
+        //usgal to usgal method
+        public static double usgal_to_Usgal(double value) {
+            return value;
+        }
+        //usgal to in3 method
+        public static double usgal_to_In3(double value) {
+            return value * 231.00038004699718722348;
+        }
+        //usgal to ukgal method
+        public static double usgal_to_Ukgal(double value) {
+            return value * 0.83267384002798812882;
+        }
+        //usgal to ft3 method
+        public static double usgal_to_Ft3(double value) {
+            return value * 0.13368077538544820832;
+        }
+        //usgal to yard2 method
+        public static double usgal_to_Yd3(double value) {
+            return value * 0.00495113994125102352;
+        }
+
+
+        //in3 to l method
+        public static double in3_to_L(double value) {
+            return value * 0.016387037037;
+        }
+        //in3 to ml method
+        public static double in3_to_Ml(double value) {
+            return value * 16.387037037 ;
+        }
+        //in3 to m3 method
+        public static double in3_to_M3(double value) {
+            return value * 0.000016387037037;
+        }
+        //in3 to Cm3 method
+        public static double in3_to_Cm3(double value) {
+            return value * 16.387037037;
+        }
+        //in3 to mm3 method
+        public static double in3_to_Mm3(double value) {
+            return value * 16387.037037;
+        }
+        //in3 to usgal method
+        public static double in3_to_Usgal(double value) {
+            return value * 0.0043289972068191047388;
+        }
+        //in3 to in3 method
+        public static double in3_to_In3(double value) {
+            return value;
+        }
+        //in3 to ukgal method
+        public static double in3_to_Ukgal(double value) {
+            return value * 0.0036046427276727789201;
+        }
+        //in3 to ft3 method
+        public static double in3_to_Ft3(double value) {
+            return value * 0.0005787037032490624176;
+        }
+        //in3 to yard2 method
+        public static double in3_to_Yd3(double value) {
+            return value * 0.0000214334709762478536;
+        }
+
+
+
+        //ukgal to l method
+        public static double ukgal_to_L(double value) {
+            return value * 4.546091880673;
+        }
+        //ukgal to Ml method
+        public static double ukgal_to_Ml(double value) {
+            return value * 4546.091880673;
+        }
+        //ukgal to m3 method
+        public static double ukgal_to_M3(double value) {
+            return value * 0.004546091880673;
+        }
+        //ukgal to Cm3 method
+        public static double ukgal_to_Cm3(double value) {
+            return value * 4546.091880673;
+        }
+        //ukgal to mm3 method
+        public static double ukgal_to_Mm3(double value) {
+            return value * 4546091.880673;
+        }
+        //ukgal to Usgal method
+        public static double ukgal_to_Usgal(double value) {
+            return value * 1.2009504225163623032952;
+        }
+        //ukgal to in3 method
+        public static double ukgal_to_In3(double value) {
+            return value * 277.4200040189031094859806;
+        }
+        //ukgal to ukgal method
+        public static double ukgal_to_Ukgal(double value) {
+            return value;
+        }
+        //ukgal to ft3 method
+        public static double ukgal_to_Ft3(double value) {
+            return value * 0.1605439836814814337904;
+        }
+        //ukgal to yard2 method
+        public static double ukgal_to_Yd3(double value) {
+            return value * 0.0059460736043835162344;
+        }
+
+
+        //ft3 to l method
+        public static double ft3_to_L(double value) {
+            return value * 28.316800022182;
+        }
+        //ft3 to ml method
+        public static double ft3_to_Ml(double value) {
+            return value * 28316.800022182;
+        }
+        //ft3 to m3 method
+        public static double ft3_to_M3(double value) {
+            return value * 0.028316800022182;
+        }
+        //ft3 to Cm3 method
+        public static double ft3_to_Cm3(double value) {
+            return value * 28316.800022182;
+        }
+        //ft3 to mm3 method
+        public static double ft3_to_Mm3(double value) {
+            return value * 28316800.022182;
+        }
+        //ft3 to usgal method
+        public static double ft3_to_Usgal(double value) {
+            return value * 7.48050717926018446663368;
+        }
+        //ft3 to in3 method
+        public static double ft3_to_In3(double value) {
+            return value * 1728.0000013535278787478004;
+        }
+        //ft3 to ukgal method
+        public static double ft3_to_Ukgal(double value) {
+            return value * 6.2288226383119958472286;
+        }
+        //ft3 to ft3 method
+        public static double ft3_to_Ft3(double value) {
+            return value;
+        }
+        //ft3 to yard2 method
+        public static double ft3_to_Yd3(double value) {
+            return value * 0.0370370378760530090096;
+        }
+
+
+        //yd3 to l method
+        public static double yd3_to_L(double value) {
+            return value * 764.553583279152;
+        }
+        //yd3 to ml method
+        public static double yd3_to_Ml(double value) {
+            return value * 764553.583279152;
+        }
+        //yd3 to m3 method
+        public static double yd3_to_M3(double value) {
+            return value * 0.764553583279152;
+        }
+        //yd3 to Cm3 method
+        public static double yd3_to_Cm3(double value) {
+            return value * 764553.583279152;
+        }
+        //yd3 to mm3 method
+        public static double yd3_to_Mm3(double value) {
+            return value * 764553583.279152;
+        }
+        //yd3 to in3 method
+        public static double yd3_to_In3(double value) {
+            return value * 46655.9989796267896230781344;
+        }
+        //yd3 to usgal method
+        public static double yd3_to_Usgal(double value) {
+            return value * 201.9736892646279059715648;
+        }
+        //yd3 to ukgal method
+        public static double yd3_to_Ukgal(double value) {
+            return value * 168.1782074246104360986096;
+        }
+        //yd3 to ft3 method
+        public static double yd3_to_Ft3(double value) {
+            return value * 26.9999993883571344573696;
+        }
+        //yd3 to yard2 method
+        public static double yd3_to_Yd3(double value) {
+            return value;
+        }
+
+
+
+    
+
 }
