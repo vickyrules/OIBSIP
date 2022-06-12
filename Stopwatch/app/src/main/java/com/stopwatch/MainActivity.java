ee02eec26989e260b430015989699ed3556b105f
vicky <vickyrules1705@gmail.com>
2022-06-12 17:13:10 +0530

    stopwatch ticking UI added started

--- Stopwatch/app/src/main/java/com/stopwatch/MainActivity.java
+++ Stopwatch/app/src/main/java/com/stopwatch/MainActivity.java
@@ -1,14 +1,279 @@
 package com.stopwatch;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    NumberPicker hrs,min,sec,millisec;
+    // on the stopwatch.
+    private int seconds = 0;
 
+    // Is the stopwatch running?
+    private boolean running;
+
+    private boolean wasRunning;
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hrs = (NumberPicker) findViewById(R.id.hrs);
+        min = (NumberPicker) findViewById(R.id.min);
+        sec = (NumberPicker) findViewById(R.id.sec);
+        millisec = (NumberPicker) findViewById(R.id.millisec);
+
+
+        if (savedInstanceState != null) {
+
+            // Get the previous state of the stopwatch
+            // if the activity has been
+            // destroyed and recreated.
+            seconds
+                    = savedInstanceState
+                    .getInt("seconds");
+            running
+                    = savedInstanceState
+                    .getBoolean("running");
+            wasRunning
+                    = savedInstanceState
+                    .getBoolean("wasRunning");
+        }
+
+
+
+        setPickerStyle(hrs);
+        setPickerStyle(sec);
+        setPickerStyle(min);
+        //setPickerStyle(millisec);
+        //millisec.setTextSize(70);
+        hrs.setMaxValue(23);
+
+        runTimer();
+
+
+
+
     }
+
+    @SuppressLint("NewApi")
+    private void setPickerStyle(NumberPicker numberPicker) {
+        numberPicker.setTextSize(210);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(59);
+        numberPicker.setMaxValue(59);
+        numberPicker.setClickable(false);
+        numberPicker.setNestedScrollingEnabled(false);
+        numberPicker.setEnabled(false);
+
+        numberPicker.setFormatter(new NumberPicker.Formatter() {
+            @Override
+            public String format(int value) {
+                NumberFormat numberFormat = new DecimalFormat("00");
+                return numberFormat.format(value);
+            }
+        });
+
+    }
+
+
+
+
+
+    // Save the state of the stopwatch
+    // if it's about to be destroyed.
+    @Override
+    public void onSaveInstanceState(
+            Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState
+                .putInt("seconds", seconds);
+        savedInstanceState
+                .putBoolean("running", running);
+        savedInstanceState
+                .putBoolean("wasRunning", wasRunning);
+    }
+
+    // If the activity is paused,
+    // stop the stopwatch.
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    // If the activity is resumed,
+    // start the stopwatch
+    // again if it was running previously.
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    // Start the stopwatch running
+    // when the Start button is clicked.
+    // Below method gets called
+    // when the Start button is clicked.
+    public void onClickStart(View view)
+    {
+        running = true;
+    }
+
+    // Stop the stopwatch running
+    // when the Stop button is clicked.
+    // Below method gets called
+    // when the Stop button is clicked.
+    public void onClickStop(View view)
+    {
+        running = false;
+    }
+
+    // Reset the stopwatch when
+    // the Reset button is clicked.
+    // Below method gets called
+    // when the Reset button is clicked.
+    public void onClickReset(View view)
+    {
+        running = false;
+        seconds = 0;
+    }
+
+    // Sets the NUmber of seconds on the timer.
+    // The runTimer() method uses a Handler
+    // to increment the seconds and
+    // update the text view.
+    private void runTimer()
+    {
+
+
+        // Creates a new Handler
+        final Handler handler
+                = new Handler();
+
+        // Call the post() method,
+        // passing in a new Runnable.
+        // The post() method processes
+        // code without a delay,
+        // so the code in the Runnable
+        // will run almost immediately.
+        handler.post(new Runnable() {
+            @Override
+
+            public void run()
+            {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                // Format the seconds into hours, minutes,
+                // and seconds.
+
+
+                changeValueByOne(sec,true);
+
+                Log.v("secs" ,seconds+"");
+                if(seconds % 60 == 0){
+                    changeValueByOne(min,true);
+                }
+                if(seconds % 3600 == 0){
+                    changeValueByOne(hrs,true);
+                }
+
+                System.out.println(seconds+"");
+
+                // If running is true, increment the
+                // seconds variable.
+                if (running) {
+
+                }
+                seconds++;
+
+                // Post the code again
+                // with a delay of 1 second.
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+
+    //changing numberpicker value auto
+    private void changeValueByOne(final NumberPicker higherPicker, final boolean increment) {
+
+        Method method;
+        try {
+            // refelction call for
+            // higherPicker.changeValueByOne(true);
+            method = higherPicker.getClass().getDeclaredMethod("changeValueByOne", boolean.class);
+            method.setAccessible(true);
+            method.invoke(higherPicker, increment);
+
+        } catch (final NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (final IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (final IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (final InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.About:
+
+                Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.Version:
+                Toast.makeText(this, "Version 1.0.0", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
